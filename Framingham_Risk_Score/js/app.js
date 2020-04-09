3860436241cc71a21e802602ab3b81e06654cc4a
Adam Zuo <adamm91046@gmail.com>
2020-04-09 09:19:58 +0800

    first commit

---
+++ Framingham_Risk_Score/js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  const NAME_LIST = ["ldl-c", "hdl-c", "bloodpressure", "diabetes", "smoke"];
+
+  $("#submit").click(function () {
+    let check = true;
+    $("input:radio").each(function () {
+      let name = $(this).attr("name");
+      if ($("input:radio[name=" + name + "]:checked").length == 0) {
+        check = false;
+      }
+    });
+
+    if (check) {
+      let gender = $("input[name='gender']:checked").val();
+
+      const age = $("#age").children("option:selected").data();
+      let result = age[gender];
+
+      console.log(result);
+
+      NAME_LIST.forEach((name) => {
+        result += $("input[name='" + name + "']:checked").data()[gender];
+      });
+
+      $("#msg").text("總分 = " + result);
+      $("article.message").removeClass("is-danger").addClass("is-success");
+    } else {
+      $("#msg").text("請勾選所有項目！");
+      $("article.message").removeClass("is-success").addClass("is-danger");
+    }
+  });
+
+  // 清空表單
+  $("#reset").click(function () {
+    $("input:radio").each(function () {
+      $(this).prop("checked", false);
+    });
+
+    $("#msg").text("勾選所有項目後點確認！");
+    $("article.message").removeClass("is-success is-danger");
+  });
+});
