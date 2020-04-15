d6b4e8cb86f7a17cd566fa4b6222f972b0a87bac
Adam Zuo <adamm91046@gmail.com>
2020-04-15 08:23:09 +0800

    separate alert display

--- BSRS-5/app.js
+++ BSRS-5/app.js
@@ -40,7 +40,9 @@ $(document).ready(function () {
 
       if ($("input[name='q" + 6 + "']:checked").val() >= 2) {
         $("#score-5").addClass("is-selected");
-      } else if (count >= 15) {
+      }
+
+      if (count >= 15) {
         $("#score-4").addClass("is-selected");
       } else if (count >= 10 && count < 15) {
         $("#score-3").addClass("is-selected");
